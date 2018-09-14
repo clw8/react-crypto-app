95310e960f89eedcbd863026f2aaa96439f13148
Chris Walsh <chris.w894j@gmail.com>
2018-09-14 23:21:47 +0200

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import AppRouter from "./app-router.js";
-
-
-ReactDOM.render( <AppRouter/>, document.getElementById('root'));
-registerServiceWorker();
